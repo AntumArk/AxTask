ffdba03a7209d60537901af7d3000bcc3d09849c
Linas Minkevičius <minkevicius.linas@gmail.com>
2024-08-22 14:24:18 +0300

    cleaned up program.cs

--- AxTask/Program.cs
+++ AxTask/Program.cs
@@ -1,44 +1,44 @@
-﻿namespace AxTask
+﻿namespace AxTask;
+
+internal class Program
 {
-    internal class Program
-    {       
-        static void Main(string[] args)
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        IDbHelper dbHelper = new DbHelper(new LogContext());
+        dbHelper.Clear();
+        var automaton = new Automaton(dbHelper);
+        var areArgsCorrect = automaton.ParseArgs(args);
+        var fileName = automaton.FileName;
+
+        if (!areArgsCorrect)
         {
-            Console.WriteLine("Hello, World!");
-            IDbHelper dbHelper = new DbHelper(new LogContext());
-            dbHelper.Clear();
-            var automaton = new Automaton(dbHelper);
-            var areArgsCorrect=automaton.ParseArgs(args);
-            var fileName= automaton.FileName;
-
-            if (!areArgsCorrect)
-            {
-                Console.WriteLine("Invalid arguments");
-                return;
-            }
-
-            var logLines = automaton.ReadFile(fileName);
-            automaton.ParseFile(logLines);
-            try
-            {
-                automaton.CheckIfColumnExists(automaton.Query);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-            automaton.LogRecords = automaton.RemoveDuplicates(automaton.LogRecords);
-            dbHelper.SaveLogRecords(automaton.LogRecords);
-            automaton.PerformQuery();
-            automaton.AlertBySeverity(10);
-
-            // Bonus
-            // todo add multiple file support
-            // todo add simple column search
-
-
-            // todo add a command line argument to specify the output file name
+            Console.WriteLine("Invalid arguments");
+            return;
         }
+
+        var logLines = automaton.ReadFile(fileName);
+        automaton.ParseFile(logLines);
+        try
+        {
+            automaton.CheckIfColumnExists(automaton.Query);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        automaton.LogRecords = automaton.RemoveDuplicates(automaton.LogRecords);
+        dbHelper.SaveLogRecords(automaton.LogRecords);
+        automaton.PerformQuery();
+        automaton.AlertBySeverity(10);
+
+        // Bonus
+        // todo add multiple file support
+        // todo add simple column search
+
+
+        // todo add a command line argument to specify the output file name
     }
 }
