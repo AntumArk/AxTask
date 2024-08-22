b1fbe7883a9768f83dd6e1222871a547c5251cd8
Linas Minkevičius <minkevicius.linas@gmail.com>
2024-08-22 14:22:36 +0300

    Removed todos

--- AxTask/Program.cs
+++ AxTask/Program.cs
@@ -32,22 +32,13 @@
             dbHelper.SaveLogRecords(automaton.LogRecords);
             automaton.PerformQuery();
             automaton.AlertBySeverity(10);
-            // todo add search by substring for each column. Return column not found if the substring is not found in the column. The query can be in any syntax, like SQL of smth like that.
-
-            // todo process the input file
-            // todo write the output file in json format.
 
             // Bonus
-            // todo boolean operator in query
             // todo add multiple file support
-            // todo add log count value in resulting JSON
+            // todo add simple column search
 
 
-            // Extra Bonus
-            // todo remove the duplicates from the output file
             // todo add a command line argument to specify the output file name
-            // todo database layer to read and write the files
-            // todo implement send alert based on severity
         }
     }
 }
