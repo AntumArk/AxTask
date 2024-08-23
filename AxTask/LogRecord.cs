8e4644a26d514c61455d114bc07530df57e2e810
Linas Minkevičius <minkevicius.linas@gmail.com>
2024-08-23 19:13:39 +0300

    Added jsonignore to id for cleaner logs

--- AxTask/LogRecord.cs
+++ AxTask/LogRecord.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace AxTask;
 
@@ -7,6 +8,7 @@ public record LogRecord
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [JsonIgnore]
     public ulong Id { get; set; }
     [Required]
     public Dictionary<string, string> RecordValues { get; set; }
