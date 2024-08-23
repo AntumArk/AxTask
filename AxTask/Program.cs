79f7bc4cbf20c56217dd9eba885c0a44aa3b6f6a
Linas Minkevičius <minkevicius.linas@gmail.com>
2024-08-23 17:34:06 +0300

    Added output arg

--- AxTask/Program.cs
+++ AxTask/Program.cs
@@ -6,23 +6,24 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-
         // Set up configuration to read from command-line arguments
         var configuration = new ConfigurationBuilder()
             .AddCommandLine(args)
             .Build();
 
         // Read the command-line arguments
-        
         var files = configuration["files"]?.Split(" ");
         var query = configuration["query"];
         var severity = configuration["alert"];
         var column = configuration["column"];
         var substring = configuration["substring"];
+        var outputFileName = configuration["output"];
 
-        if (files == null || 
-            files.Length == 0 || 
-            !IsQueryProvided(query,column,substring))
+
+        if (files == null ||
+            files.Length == 0 ||
+            !IsQueryProvided(query, column, substring) ||
+            !IsOutputFilePathCorrect(outputFileName))
         {
             PrintHelp();
             return;
@@ -55,8 +56,14 @@ internal class Program
         // todo add multiple file support
         // todo add simple column search
 
-
         // todo add a command line argument to specify the output file name
+    }
+
+    private static bool IsOutputFilePathCorrect(string? outputFileName)
+    {
+        if (string.IsNullOrEmpty(outputFileName)) return false;
+        var outputDirectory = Path.GetDirectoryName(outputFileName);
+        return Directory.Exists(outputDirectory);
     }
 
     private static bool IsQueryProvided(string? query, string? column, string? substring)
@@ -71,10 +78,11 @@ internal class Program
         Console.WriteLine("Usage: AxTask [options]");
         Console.WriteLine();
         Console.WriteLine("Options:");
-        Console.WriteLine("  --files \"file1 file2 ...\"      Specifies the list of CSV files to process.");
-        Console.WriteLine("  --query \"<query>\"              Specifies the SQL query to execute.");
-        Console.WriteLine("  --column \"<column>\"            Specifies the column to search.");
-        Console.WriteLine("  --substring \"<substring>\"      Specifies the substring to search for in the specified column.");
+        Console.WriteLine("  --files \"file1 file2 ...\"      Specifies the list of CSV files to process (required).");
+        Console.WriteLine("  --query \"<query>\"              Specifies the SQL query to execute (required).");
+        Console.WriteLine("  --column \"<column>\"            Specifies the column to search (required).");
+        Console.WriteLine("  --substring \"<substring>\"      Specifies the substring to search for in the specified column (required).");
+        Console.WriteLine("  --output <filename>             Specifies the output file name (required).");
         Console.WriteLine("  --alert <severity>              Specifies the alert severity level (optional).");
         Console.WriteLine();
         Console.WriteLine("Note: You must provide either the --query argument or both the --column and --substring arguments.");
@@ -82,8 +90,9 @@ internal class Program
         Console.WriteLine("Examples:");
         Console.WriteLine("  AxTask --files \"file1.csv file2.csv\" --query \"SELECT * FROM LogRecords WHERE RecordValues->>'signatureId' LIKE '%4608%'\"");
         Console.WriteLine("  AxTask --files \"file1.csv file2.csv\" --column \"signatureId\" --substring \"4608\"");
-        Console.WriteLine("  AxTask --files \"file1.csv file2.csv\" --query \"SELECT * FROM LogRecords\" --alert 10");
+        Console.WriteLine("  AxTask --files \"file1.csv file2.csv\" --query \"SELECT * FROM LogRecords\" --alert 10 --output \"output.json\"");
     }
+
     public static bool ParseArgs(string[] args)
     {
         if (args.Length != 2)
